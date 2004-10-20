5ed52fdd481c302977d1b06b8f4594e73f5285b5
sdimitro <>
2004-10-20 20:18:12 +0000

    fix for black text area in line graph jpeg

--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/performance/graph/LineGraph.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/performance/graph/LineGraph.java
@@ -91,7 +91,7 @@ public class LineGraph {
         int right= bounds.width - lastItem.getSize(g).x - PADDING;
 
         // draw the title
-        g.drawString(fTitle, (bounds.width - titleWidth) / 2, top);
+        g.drawString(fTitle, (bounds.width - titleWidth) / 2, top, true);
         
         // draw the max and min values
         g.drawString(smin, PADDING/2+labelWidth-emin.x, bottom-titleHeight, true);
