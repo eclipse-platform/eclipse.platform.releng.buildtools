306543e9342e3bbfe2ce830627ba26fbb157f21d
sdimitro <>
2004-10-22 21:13:37 +0000

    *** empty log message ***

--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/performance/graph/Grapher.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/performance/graph/Grapher.java
@@ -16,6 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
 import junit.framework.AssertionFailedError;
 
@@ -83,14 +84,15 @@ public class Grapher {
 	                    String buildID= ts.getLabel(j);
 	                    double value= ts.getValue(j);
 	                    Color c= buildID.indexOf(referenceBuildId)  >= 0 ? green : black;
-	                    if (c == green || n-2<j)
-	                    	graph.addItem(buildID, dim.getDisplayValue(value), value, c);
-	                    else
-	                    	graph.addItem(buildID, value, c);
+	                    if (c == green)
+	                    	graph.addItem(buildID, dim.getDisplayValue(value), value, c, true);
+	                    if (!buildID.startsWith("N"))
+	                    	c = display.getSystemColor(SWT.COLOR_DARK_BLUE);
+	                    graph.addItem(buildID, dim.getDisplayValue(value), value, c, (n-2<j) );	
 	                }
 	                
 	                drawGraph(graph, outputDirectory + "/" + scenarioName.replace('#', '.').replace(':','_').replace('\\','_') + "_" + dimensionName);
-                }
+	                }
                 } catch (AssertionFailedError e){
                 	//System.err.println("Unable to get result for: "+t.getScenarioName()+" "+ts.toString());
                 }
@@ -112,6 +114,52 @@ public class Grapher {
             out= new BufferedOutputStream(new FileOutputStream(output + ".jpeg"));
             //System.out.println("writing: " + output);
             il.save(out, SWT.IMAGE_JPEG);
+            
+            String areas= p.getAreas();
+            String scenarioName=output.substring(output.lastIndexOf('/')+1);
+            if (areas != null) {
+    	        try {
+    	            PrintStream os= new PrintStream(new FileOutputStream(output + ".html"));
+     	            os.println("<html><body>");
+    	            os.println("<script language=\"JavaScript\">");
+    	            os.println("if (!document.layers&&!document.all)");
+    	            os.println("event=\"test\"");
+    	            os.println("function showtip(current,e,text){");
+    	            os.println("if (document.all){");
+    	            os.println("thetitle=text.split('<br>')");
+    	            os.println("if (thetitle.length>1){");
+    	            os.println("thetitles=''");
+    	            os.println("for (i=0;i<thetitle.length;i++)");
+    	            os.println("thetitles+=thetitle[i]");
+    	            os.println("current.title=thetitles}");
+    	            os.println("else");
+    	            os.println("current.title=text}");
+    	            os.println("else if (document.layers){");
+    	            os.println("document.tooltip.document.write('<layer bgColor=\"white\" style=\"border:1px solid black;font-size:12px;\">'+text+'</layer>')");
+    	            os.println("document.tooltip.document.close()");
+    	            os.println("document.tooltip.left=e.pageX+5");
+    	            os.println("document.tooltip.top=e.pageY+5");
+    	            os.println("document.tooltip.visibility=\"show\"}}");
+    	            os.println("function hidetip(){");
+    	            os.println("if (document.layers)");
+    	            os.println("document.tooltip.visibility=\"hidden\"}");
+    	            os.println("</script>");
+    	            os.println("<div id=\"tooltip\" style=\"position:absolute;visibility:hidden\"></div>");
+
+    	            
+    	            os.println("<img src=\"" + scenarioName + ".jpeg\" usemap=\"#" + scenarioName + "\">");
+    	            os.println("<map name=\"" + scenarioName + "\">");
+    	            os.println(areas);
+    	            os.println("</map>");
+    	            os.println("</body></html>");
+    	            os.close();
+    	        } catch (FileNotFoundException e) {
+    	            // TODO Auto-generated catch block
+    	            e.printStackTrace();
+    	        }
+            }
+            
+            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
