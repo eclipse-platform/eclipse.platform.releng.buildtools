c20bb53d1bdf5210f64f2db607bf168d34930420
kmoir <>
2005-11-07 18:43:27 +0000

    3.2M3 plugins

--- bundles/org.eclipse.test.performance.ui/scripts/genresults.bat
+++ bundles/org.eclipse.test.performance.ui/scripts/genresults.bat
@@ -2,5 +2,5 @@
 
 REM set dbloc=net://trelenggtk.ottawa.ibm.com
 
-java -Declipse.perf.dbloc=%dbloc% -jar .\..\..\..\startup.jar -os win32 -ws win32 -arch x86 -application org.eclipse.test.performance.ui.resultGenerator %*
+java -Declipse.perf.dbloc=%dbloc% -jar .\..\..\..\startup.jar -application org.eclipse.test.performance.ui.resultGenerator %*
 
