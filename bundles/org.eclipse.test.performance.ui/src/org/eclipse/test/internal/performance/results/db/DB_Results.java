cc1194dcc899ea36a57c2630c2aef9c6e85aede5
ffusier <>
2010-07-08 14:24:35 +0000

    R3_6_maintenance - 319274

--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/internal/performance/results/db/DB_Results.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/internal/performance/results/db/DB_Results.java
@@ -657,7 +657,7 @@ public static String getLastBaselineBuild(String date) {
 	String lastBaselineBuild = null;
 	for (int i=0; i<BUILDS_LENGTH; i++) {
 		String build = BUILDS[i];
-		if (build.startsWith(DB_BASELINE_PREFIX)) {
+		if (build.startsWith(DB_VERSION_REF)) {
 			String buildDate = build.substring(build.indexOf('_')+1);
 			if (buildDate.compareTo(date) < 0) {
 				if (lastBaselineBuild == null || build.compareTo(lastBaselineBuild) > 0) {
