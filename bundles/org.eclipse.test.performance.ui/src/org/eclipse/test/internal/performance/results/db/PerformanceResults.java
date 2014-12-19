2afbdeb7378f9a8ddb28b2fb3358ff18d806c045
David Williams <david_williams@us.ibm.com>
2014-12-18 20:58:39 -0500

    Bug 455656 - Error in performance results analysis, if more recent build exists in database.

--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/internal/performance/results/db/PerformanceResults.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/internal/performance/results/db/PerformanceResults.java
@@ -670,6 +670,10 @@ private void setAllBuildNames() {
 		if (n < buildsSize) {
 			System.arraycopy(this.allBuildNames, 0, this.allBuildNames = new String[n], 0, n);
 		}
+		// there are times when the build we want to analyze is not the "last build", 
+		// such as there may have be subsequent builds, already. 
+		// so, we only compute this "default behavior" if lastBuild has not been set yet.
+		// 
 		int idx = n-1;
 		String lastBuild = this.allBuildNames[idx--];
 		while (idx > 0 && lastBuild.startsWith(DB_Results.getDbBaselinePrefix())) {
@@ -677,6 +681,7 @@ private void setAllBuildNames() {
 		}
 		this.needToUpdateLocalFile = this.name == null || Util.getBuildDate(lastBuild).compareTo(Util.getBuildDate(this.name)) > 0;
 		this.name = lastBuild;
+		
 		if (this.baselineName != null) {
 			String lastBuildDate = Util.getBuildDate(lastBuild);
 			if (Util.getBuildDate(this.baselineName).compareTo(lastBuildDate) > 0) {
