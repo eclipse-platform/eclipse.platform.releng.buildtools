fc4a4cc2fa612ea8748cbb42acbd39f918e1a8ab
Alexander Kurtakov <akurtako@redhat.com>
2018-08-14 12:39:47 +0300

    Bug 531057- JUnit 5 support for tests
    Adjust results generator for the change in ant junitlauncher from errors
    to aborted element in xml.
    
    Change-Id: Ifb7af115fa14e836b4aaad73d261dcc8a0dd23bf
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.build.tools/src/org/eclipse/releng/generators/TestResultsGenerator.java
+++ bundles/org.eclipse.build.tools/src/org/eclipse/releng/generators/TestResultsGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others. All rights reserved.
+ * Copyright (c) 2000, 2018 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -458,7 +458,10 @@ public class TestResultsGenerator extends Task {
                         for (int i = 0; i < elementCount; i++) {
                             final Element element = (Element) elements.item(i);
                             final NamedNodeMap attributes = element.getAttributes();
-                            Node aNode = attributes.getNamedItem("errors");
+                            Node aNode = attributes.getNamedItem("aborted");
+                            if (aNode == null) {
+                                attributes.getNamedItem("errors");
+                            }
                             errorCount = errorCount + Integer.parseInt(aNode.getNodeValue());
                             aNode = attributes.getNamedItem("failures");
                             errorCount = errorCount + Integer.parseInt(aNode.getNodeValue());
