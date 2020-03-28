c8a361ffa1e4a130c63d6ff4e911ef8ab8a6cd5c
Alexander Kurtakov <akurtako@redhat.com>
2020-03-28 07:24:04 +0200

    Fix generics warnings.
    Change-Id: I574d543f4937b6b2a88715952b4428e414370595
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/performance/ui/RawDataTable.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/performance/ui/RawDataTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ private RawDataTable(ConfigResults results, PrintStream ps) {
 	this.stream = ps;
 }
 
-public RawDataTable(ConfigResults results, List prefixes, PrintStream ps) {
+public RawDataTable(ConfigResults results, List<String> prefixes, PrintStream ps) {
 	this(results, ps);
 	this.buildPrefixes = prefixes;
 }
--- bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/performance/ui/ScenarioData.java
+++ bundles/org.eclipse.test.performance.ui/src/org/eclipse/test/performance/ui/ScenarioData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,7 +63,7 @@ public class ScenarioData {
  * @param outputDir The directory root where the files are generated
  *
 */
-public ScenarioData(String baselinePrefix, List<String> pointsOfInterest, List buildIDPatterns, File outputDir) {
+public ScenarioData(String baselinePrefix, List<String> pointsOfInterest, List<String> buildIDPatterns, File outputDir) {
 	this.baselinePrefix = baselinePrefix;
 	this.pointsOfInterest = pointsOfInterest;
 	this.buildIDStreamPatterns = buildIDPatterns;
